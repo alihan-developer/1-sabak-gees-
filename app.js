80bc91361cc23898c58042498185164c80d95b42
alihan-developer <janybekovalihan2016@gmail.com>
2024-11-21 09:40:25 +0600

    add

---
+++ app.js
@@ -0,0 +1,28 @@
+//  let var columnRuleStyle
+// var, ESS
+//let const ES6 (EsmaScript)
+
+//  const myName='alihan'
+//  let myAge=11
+//  const myLive = 'я живу в джале'
+
+// let message='Hello ' +myName+ ' ' +myAge+'Wellcome to JS'  //ES5
+//  let newMessage=`Hello ${myName} Wellcome to JS`  //ES6
+//  console.log(message);
+//  console.log(newMessage);
+
+const userName=prompt('What your name?')
+const isMerried=confirm('Now old are you')
+alert=(userName)
+
+
+
+
+//  const num= parseInt(prompt('first sun'))
+//  const num2= parseInt(prompt('second number'))
+//  const summa= num+num2
+//  console.log(summa);
+//  const minus=num-num2
+//  console.log(num+'-'+num2+'='+minus);
+//  const umnoj=num*num2 
+//  console.log(umnoj);
